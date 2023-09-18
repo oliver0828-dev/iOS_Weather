721771b5037486d38eafe708ca57310234f353b8
Oliver Park <oliverco0828@gmail.com>
2023-09-18 21:35:54 +0900

    Initial Commit

---
+++ iOS_Weather/iOS_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_WeatherApp.swift
+//  iOS_Weather
+//
+//  Created by Oliver Park on 2023/09/18.
+//
+
+import SwiftUI
+
+@main
+struct iOS_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
